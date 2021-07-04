0d4b04c43c3273a41e94d27e4e12396563513708
ruida <ruidanielribeiro@gmail.com>
2021-07-04 13:04:34 +0100

    Working Solution: the only thing I missed was the Service interface (only did the implementation initially.

---
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring5jokesappv2.controllers;
+
+import guru.springframework.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping
+    public String getRandomJoke(Model model){
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -0,0 +1,18 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService{
+
+    private ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    public String getJoke(){
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
